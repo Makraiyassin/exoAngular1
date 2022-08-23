c4616f53368be0dce765574c1dcfcd0711c8acf9
Yassin Makrai <makrai.yassin@gmail.com>
2022-08-24 01:14:06 +0200

    exo tuteur detail + list

--- src/app/pages/tuteur/components/tuteur-detail/tuteur-detail.component.ts
+++ src/app/pages/tuteur/components/tuteur-detail/tuteur-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable} from "rxjs";
 import {Tuteur} from "../../models/tuteur.dto";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {ActivatedRoute} from "@angular/router";
 
 @Component({
@@ -13,13 +13,21 @@ export class TuteurDetailComponent implements OnInit {
   private getOne$: Observable<Tuteur> | null = null;
   private _tuteur: Tuteur | null = null;
 
+  localUrl = "http://localhost:8080"
+  remoteUrl = "https://exo-nursery.herokuapp.com"
+
+  token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhIiwicm9sZXMiOlsiUk9MRV9QRVJTT05ORUwiXSwiZXhwIjoxNjYxMzc5MDE2fQ.VGA_voBmgjNjpoGNetPsmfium60vxzR9ftbrArafl-spqH07MNGDr36ZIt9NmPpMOuqcPSVSXwGAF-JcxqT72g"
+  params = new HttpHeaders()
+    .append("Authorization", this.token)
+
   get tuteur(): Tuteur | null {
     return this._tuteur;
   }
 
   constructor(private _http: HttpClient, private _route: ActivatedRoute) {
     this._route.paramMap.subscribe(map => {  // paramMap, subscribe ???????????
-      this.getOne$ = this._http.get<Tuteur>(`https://exo-nursery.herokuapp.com//tuteur/${map.get("id")}`)
+
+      this.getOne$ = this._http.get<Tuteur>(`${this.localUrl}/tuteur/${map.get("id")}`, {headers: this.params})
       this.getOne$.subscribe(tuteur => this._tuteur = tuteur);
     })
   }
--- src/app/pages/tuteur/components/tuteur-list/tuteur-list.component.ts
+++ src/app/pages/tuteur/components/tuteur-list/tuteur-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable} from "rxjs";
 import {Tuteur} from "../../models/tuteur.dto";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
   selector: 'app-tuteur-list',
@@ -12,12 +12,19 @@ export class TuteurListComponent implements OnInit {
   private getAll$: Observable<any[]> // observable ??????????????
   private _tuteurs: Tuteur[] = [];
 
+  localUrl = "http://localhost:8080"
+  remoteUrl = "https://exo-nursery.herokuapp.com"
+
+  token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhIiwicm9sZXMiOlsiUk9MRV9QRVJTT05ORUwiXSwiZXhwIjoxNjYxMzc5MDE2fQ.VGA_voBmgjNjpoGNetPsmfium60vxzR9ftbrArafl-spqH07MNGDr36ZIt9NmPpMOuqcPSVSXwGAF-JcxqT72g"
+  params = new HttpHeaders()
+    .append("Authorization", this.token)
+
   get tuteurs(): Tuteur[] {
     return this._tuteurs;
   }
 
   constructor(private _http: HttpClient) {
-    this.getAll$ = this._http.get<Tuteur[]>("https://exo-nursery.herokuapp.com/tuteur/all");
+    this.getAll$ = this._http.get<Tuteur[]>(`${this.localUrl}/tuteur/all`, {headers: this.params});
   }
 
   ngOnInit(): void {
